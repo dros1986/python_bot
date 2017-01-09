00fd7e50cacd0839df10e053f6b3477ed78ce492
Flavio Piccoli <flavio_86@yahoo.com>
2017-01-09 12:25:54 +0100

    Matlab example is now complete. Needs to be tested in Windows.

--- matlab_example.py
+++ matlab_example.py
@@ -1,7 +1,31 @@
 from Updater import Updater
+import os, sys, platform
+
+def fileparts(fn):
+    (dirName, fileName) = os.path.split(fn)
+    (fileBaseName, fileExtension) = os.path.splitext(fileName)
+    return dirName, fileBaseName, fileExtension
+
 
 def imageHandler(bot, message, chat_id, local_filename):
-	print(local_filename)
+	# send message to user
+	bot.sendMessage(chat_id, "Hi, please wait until the image is ready")
+	# set matlab command
+	if 'Linux' in platform.system():
+		matlab_cmd = 'matlab'
+	else:
+		matlab_cmd = '"C:\\Program Files\\MATLAB\\R2014b\\bin\\matlab.exe"'
+	# set command to start matlab script "edges.m"
+	cmd = matlab_cmd + " -nodesktop -nosplash -r \"edges(\'" + local_filename + "\'); quit\""
+	print(cmd)
+	# lunch command
+	os.system(cmd)
+	# send back the manipulated image
+	dirName, fileBaseName, fileExtension = fileparts(local_filename)
+	new_fn = os.path.join(dirName, fileBaseName + '_ok' + fileExtension)
+	bot.sendImage(chat_id, new_fn, "")
+
+
 
 if __name__ == "__main__":
 	bot_id = '128366843:AAHovviK9AQDbcWJkM9JkqDAt8B5oLUUCQI'
